d638c5a36979a2896e615ed9b55ea49b551e78b7
lperalta <leonardo.peralta@mirgor.com.ar>
2022-11-14 20:25:54 -0300

    Desafio: Motores de plantillas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const { json } = require('express');
 const express = require ('express');
 
+const Contenedor = require('./models/ProductContenedor');
+const prueba = new Contenedor ();
+
 const app = express();
 const PORT = 8080;
 
@@ -19,4 +22,32 @@ const userRouter = require('./routers/users');
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 
+/* 
+// Comentado para aplicar el nuevo desafio
 app.use('/', express.static('public'));
+ */
+
+// Indica el motor de plantillas a utilizar (utiliza por defecto la carpeta ./views)
+app.set('view engine', 'ejs');
+
+// Endpoint del /hola en donde se rederizará la plantilla 'index'
+app.get('/', (req,res) => {
+    res.render('pages/index');
+});
+
+// Endpoint para el POST /productos
+app.post('/productos', (req,res) =>{
+    let producto = {
+        title: req.body.title,
+        price: parseInt(req.body.price),
+        thumbnail: req.body.thumbnail,
+    };
+    console.log('Se crea el producto', producto);
+    prueba.save(producto);
+    res.redirect('/');
+});
+
+app.get('/productos', (req,res) => {
+    let listaProductos = prueba.getAll();
+    res.render('pages/productos', {lista: listaProductos});
+});
